b18bf554739087cdc30835d67bda7380d6e11809
joao guilherme <joaoguilhermehig@gmail.com>
2023-09-14 22:09:51 -0300

    Um toast quando clica na galeria

--- app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
@@ -5,17 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.card.MaterialCardView;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
-    MaterialCardView cardAeromodelos;
+    MaterialCardView cardAeromodelos, cardGaleria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_principal_layout);
         cardAeromodelos = findViewById(R.id.cardAeromodelos);
+        cardGaleria = findViewById(R.id.cardGaleria);
+
+        cardGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "cliquei em galeria", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         cardAeromodelos.setOnClickListener(new View.OnClickListener() {
             @Override
