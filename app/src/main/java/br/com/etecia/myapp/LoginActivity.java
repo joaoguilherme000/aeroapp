4b614bf02d344a98a9bc23f4e0a1ba6034e3e328
joaoguilherme <joaoguilhermehig@gmail.com>
2023-09-21 19:43:39 -0300

    clique indo para main

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -2,13 +2,29 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
+    //declarar uma variavel global do tipo objeto no layout
+
+    Button btnSign; // tipo
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        btnSign = findViewById(R.id.btnSigin); // Encontre o botão "Sign In" pelo ID
+        btnSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
     }
 }
