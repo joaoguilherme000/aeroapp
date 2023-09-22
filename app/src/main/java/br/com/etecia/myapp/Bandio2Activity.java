ff61720b121f56df0c1a5989a8a27b1d3206e738
joaoguilherme <joaoguilhermehig@gmail.com>
2023-09-21 22:00:32 -0300

    varias telas faceis

---
+++ app/src/main/java/br/com/etecia/myapp/Bandio2Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bandio2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bandio2_layout);
+    }
+}
